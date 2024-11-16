d9e26acc0c8438b097858bd536055dcb3370d105
JuanD <gutirrezvidalj@gmail.com>
2024-11-16 16:53:48 -0500

    Feat:(Data Base and CRUD Shopping car)

--- src/main/java/com/rojas/dev/XCampo/repository/OrderRepository.java
+++ src/main/java/com/rojas/dev/XCampo/repository/OrderRepository.java
@@ -1,14 +1,12 @@
 package com.rojas.dev.XCampo.repository;
 
 import com.rojas.dev.XCampo.entity.Order;
-import com.rojas.dev.XCampo.entity.Product;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
